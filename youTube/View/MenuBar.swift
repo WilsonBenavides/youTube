c0220806c5e76b869c7f00464c6d1f6d7481d46c
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-05-08 16:12:04 -0700

    blue bar added

---
+++ youTube/View/MenuBar.swift
@@ -0,0 +1,22 @@
+//
+//  MenuBar.swift
+//  youTube
+//
+//  Created by willix on 8/05/18.
+//  Copyright © 2018 willix. All rights reserved.
+//
+
+import UIKit
+
+class MenuBar: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = UIColor.rgb(red: 230, green: 32, blue: 31)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
