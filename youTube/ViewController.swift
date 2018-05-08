02dd89ff4ed354be24ca696626ea95d02a1c0854
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-05-08 06:05:06 -0700

    red background color again

---
+++ youTube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  youTube
+//
+//  Created by willix on 7/05/18.
+//  Copyright © 2018 willix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+    }
+}
+
+
+
+
+
+
+
+
