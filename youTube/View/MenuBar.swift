06e1cb4d75ca8e50f3366afa21a2343d0dc2f68d
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-05-08 18:35:47 -0700

    change color when click item

--- youTube/View/MenuBar.swift
+++ youTube/View/MenuBar.swift
@@ -70,7 +70,15 @@ class MenuCell: BaseCell {
     
     override var isHighlighted: Bool {
         didSet {
-            print(123)
+            imageView.tintColor = isHighlighted ? UIColor.white : UIColor.rgb(red: 91, green: 14, blue: 13)
+            print("is highlithted")
+        }
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+            imageView.tintColor = isSelected ? UIColor.white : UIColor.rgb(red: 91, green: 14, blue: 13)
+            print("is selected")
         }
     }
     
